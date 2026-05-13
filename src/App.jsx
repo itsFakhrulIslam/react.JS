cc6a7703617f96e95c404c66907347faf6486192
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-13 20:37:34 +0600

    jsx explore with function

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,26 @@ function App() {
       <section id="center">
         <div>
           <h1>Lets Learn React Js</h1>
+
+          <TestPerson />
         </div>
       </section>
     </>
   );
+}
+
+function TestPerson() {
+  const name = "miraj";
+  const age = 23;
+  return (
+    <p>
+      I am a {name}, {age} years old, <Habit />
+    </p>
+  );
+}
+
+function Habit() {
+  return <p>i have learn React or new technology</p>;
 }
 
 export default App;
