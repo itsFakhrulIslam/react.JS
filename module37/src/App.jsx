d220ce650a089b1c4a5488f891922cd5994d4ffb
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-14 16:36:07 +0600

    event handling in react is the most important

--- module37/src/App.jsx
+++ module37/src/App.jsx
@@ -1,11 +1,54 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("poke 1 clicked");
+  }
+
+  const handleClick2 = () => {
+    alert("poke 3 clicked");
+  };
+
+  const handleClick4 = (num) => {
+    let result = num * 5;
+    alert(result);
+  };
+
   return (
     <>
       <section id="center">
         <div>
-          <h1>Learn ReactJs module37</h1>
+          <h2>Learn ReactJs module37</h2>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: "5px",
+            }}
+          >
+            <button onClick={handleClick}>Poke me 1</button>
+
+            <button onClick={(handleClick1) => alert("poke 2 clicked")}>
+              Poke me 2
+            </button>
+
+            <button onClick={handleClick2}>Poke me 3</button>
+
+            <button
+              onClick={function handleClick3() {
+                alert("poke 4 clicked");
+              }}
+            >
+              Poke me 4
+            </button>
+
+            {/* wrap by arrow function for pass the parameters */}
+            <button onClick={() => handleClick4(10)}>
+              Pass params by click
+            </button>
+          </div>
         </div>
       </section>
     </>
