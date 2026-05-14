d57e6801bd861c08a218a97dd27b6008497680ce
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-14 07:57:08 +0600

    coditinal rendering way 6, this part is most important, m36.7 is done

--- src/Task.jsx
+++ src/Task.jsx
@@ -46,6 +46,52 @@
 // }
 
 // pending
+// export default function Task({ taskName, taskIsDone }) {
+//   return <p>{taskIsDone || taskName}</p>;
+// }
+
+// ternary
+// export default function Task({ taskName, taskIsDone }) {
+//   return taskIsDone ? <li>Done: {taskName}</li> : <li>Pending: {taskName}</li>;
+// }
+
+// and &&
+// export default function Task({ taskName, taskIsDone }) {
+//   return taskIsDone && <li>Done: {taskName}</li>;
+// }
+
+// and ||
+// export default function Task({ taskName, taskIsDone }) {
+//   return taskIsDone || <li>Pending: {taskName}</li>;
+// }
+
+//
+// export default function Task({ taskName, taskIsDone }) {
+//   if (taskIsDone === true) {
+//     return <p>Done: {taskName}</p>;
+//   } else {
+//     return <p>Pending: {taskName}</p>;
+//   }
+// }
+
+//
+// export default function Task({ taskName, taskIsDone }) {
+//   if (taskIsDone === true) {
+//     return null;
+//   } else {
+//     return <p>Pending: {taskName}</p>;
+//   }
+// }
+
+//
 export default function Task({ taskName, taskIsDone }) {
-  return <p>{taskIsDone || taskName}</p>;
+  let listItem;
+
+  if (taskIsDone === true) {
+    listItem = <p>Pending: {taskName}</p>;
+  } else {
+    listItem = <p>Pending: {taskName}</p>;
+  }
+
+  return listItem;
 }
