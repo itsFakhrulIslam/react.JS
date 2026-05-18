74ff4d990189fd709cca9f8184a183ab98cdb6ba
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-18 12:57:07 +0600

    something changed here

--- module38/src/Test.jsx
+++ module38/src/Test.jsx
@@ -1,8 +1,11 @@
 export default function Test() {
-    return (
-        <>
-            <h1>Test</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Test</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+        voluptate.
+      </p>
+    </>
+  );
 }
