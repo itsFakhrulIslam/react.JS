6d06cd4f3851eaddb61a47d82991f4fdccd9c337
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-20 21:07:31 +0600

    previous code collaps for new code adding

--- module39/src/App.jsx
+++ module39/src/App.jsx
@@ -1,36 +1,38 @@
 import "./App.css";
-import {
-  add,
-  divide,
-  greetNewMember,
-  modulus,
-  multiply,
-  sayHelloToNewMember,
-  subtract,
-} from "./utils/math/math";
+// import {
+//   add,
+//   divide,
+//   greetNewMember,
+//   modulus,
+//   multiply,
+//   sayHelloToNewMember,
+//   subtract,
+// } from "./utils/math/math";
 
 function App() {
-  const sumation = add(5, 10);
-  console.log("sumation", sumation);
+  //^ import/export example
+  // const sumation = add(5, 10);
+  // console.log("sumation", sumation);
 
-  const substraction = subtract(10, 5);
-  console.log("substraction", substraction);
+  // const substraction = subtract(10, 5);
+  // console.log("substraction", substraction);
 
-  const multiplication = multiply(5, 10);
-  console.log("multiplication", multiplication);
+  // const multiplication = multiply(5, 10);
+  // console.log("multiplication", multiplication);
 
-  const division = divide(10, 5);
-  console.log("division", division);
+  // const division = divide(10, 5);
+  // console.log("division", division);
 
-  const modulusation = modulus(10, 3);
-  console.log("modulus", modulusation);
+  // const modulusation = modulus(10, 3);
+  // console.log("modulus", modulusation);
 
-  const sayGreeting = greetNewMember("miraj");
-  console.log("greetNewMember", sayGreeting);
+  // const sayGreeting = greetNewMember("miraj");
+  // console.log("greetNewMember", sayGreeting);
 
-  const sayHello = sayHelloToNewMember("miraj");
-  console.log("sayHelloToNewMember", sayHello);
+  // const sayHello = sayHelloToNewMember("miraj");
+  // console.log("sayHelloToNewMember", sayHello);
 
+  
   return (
     <>
       <section id="center">
