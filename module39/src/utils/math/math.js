e225df36090dd368b5130f85df5683137b7c2c3a
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-20 21:05:55 +0600

    learn import export in es6 modules

--- module39/src/utils/math/math.js
+++ module39/src/utils/math/math.js
@@ -2,5 +2,22 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const modulus = (a, b) => a % b;
 
-export { add, subtract, multiply, divide };
+const welcomeToNewMemberLearnReactJsModule39 = (name) => {
+  console.log(`Welcome to new member learn ReactJs module39, ${name}!`);
+};
+
+const sayHelloToNewMemberLearnReactJsModule39 = (name) => {
+  console.log(`Hello to new member learn ReactJs module39, ${name}!`);
+};
+
+export {
+  add,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  welcomeToNewMemberLearnReactJsModule39 as greetNewMember,
+  sayHelloToNewMemberLearnReactJsModule39 as sayHelloToNewMember,
+};
---
+++ module39/src/utils/tax/tax.js
@@ -0,0 +1,5 @@
+import { multiply } from "../math/math";
+
+const calculateTax = multiply(0.1, 10000); // Example tax calculation
+
+export { calculateTax };
