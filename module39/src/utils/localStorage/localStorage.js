a80e47042516fe4570f6a9cc497748dc06927faa
fimsHere <gmtelecom9089@gmail.com>
2026-05-22 23:30:36 +0600

    local storage use to stored bottles data for the next step to buy

---
+++ module39/src/utils/localStorage/localStorage.js
@@ -0,0 +1,35 @@
+// ^get items from the local storage
+const getToLocalStorage = () => {
+  const storedCartString = localStorage.getItem("cart");
+
+  if (storedCartString) {
+    const storedCart = JSON.parse(storedCartString);
+
+    return storedCart;
+  }
+
+  return [];
+};
+
+// ^set items to the local storage
+const setToLocalStorage = (id) => {
+  const getCart = getToLocalStorage();
+
+  // first way to add
+  //   const newCart = [...getCart, id];
+
+  // another way to add
+  getCart.push(id);
+
+  // save newCart/getCart to the local storage
+  saveCartLocalStorage(getCart);
+};
+
+// save cart to local storage
+const saveCartLocalStorage = (cart) => {
+  const cartStringify = JSON.stringify(cart);
+
+  localStorage.setItem("cart", cartStringify);
+};
+
+export { getToLocalStorage, setToLocalStorage };
