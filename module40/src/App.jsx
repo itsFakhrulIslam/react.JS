7ab4d79537ec5b3f6ebf5a724c3798dd02b77fa1
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-23 14:43:04 +0600

    tailwind css added and setup ready to project

--- module40/src/App.jsx
+++ module40/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <section id="center">
         <div>
-          <h1>Learn ReactJs module40</h1>
+          <h1 className="text-center text-2xl">Learn ReactJs module40</h1>
         </div>
       </section>
     </>
