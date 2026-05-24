1c519b5fcf0ee8db181990d8c07b867e87d9db80
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-24 17:59:05 +0600

    test by daisy ui nav

--- module40/src/App.jsx
+++ module40/src/App.jsx
@@ -3,11 +3,29 @@ import "./App.css";
 function App() {
   return (
     <>
-      <section id="center">
-        <div>
-          <h1 className="text-center text-2xl">Learn ReactJs module40</h1>
+      <div class="navbar bg-base-100 shadow-sm">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl">daisyUI</a>
         </div>
-      </section>
+        <div class="flex-none">
+          <button class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block h-5 w-5 stroke-current"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>{" "}
+            </svg>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
