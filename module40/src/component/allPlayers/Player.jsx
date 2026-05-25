25571abc1de3af7ac97392822e098d54bf584d28
fimsHere <gmtelecom9089@gmail.com>
2026-05-25 08:10:52 +0600

    card tamplate ready for reusable component

--- module40/src/component/allPlayers/Player.jsx
+++ module40/src/component/allPlayers/Player.jsx
@@ -1,27 +1,54 @@
-import image_url from "../../assets/hero.png";
+import userLogo from "../../assets/user.png";
+import flagLogo from "../../assets/flags.png";
 
 const Player = ({ onPlayer }) => {
-  //   console.log(onPlayer);
-
-  const { name, bat_side, bowl_side, country, price } = onPlayer;
+  console.log(onPlayer);
 
   return (
     <>
       <div>
-        {onPlayer.map((player, index) => (
-          <div key={index} className="card bg-base-100 w-96 shadow-sm">
-            <figure>
-              <img src={image_url} alt={`players ${name}`} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{name}</h2>
-              <p>Batting Side: {bat_side}</p>
-              <p>Bowling Side: {bowl_side}</p>
-              <p>Country: {country}</p>
-              <p>Price: {price}</p>
+        <div className="card bg-white text-black w-96 shadow-sm p-6">
+          <figure>
+            <img
+              className="rounded-lg"
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              alt="Shoes"
+            />
+          </figure>
+
+          <div className="border-b border-gray-300 py-2">
+            <div className="flex items-center">
+              <img className="w-10 -ml-2" src={userLogo} alt="user" />
+              <h2 className="card-title">Virat Kohli</h2>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <img className="w-7 -ml-1" src={flagLogo} alt="flagLogo" />
+                <h3>CountryName</h3>
+              </div>
+
+              <button className="btn">All rounder</button>
             </div>
           </div>
-        ))}
+
+          <div className="space-y-2">
+            <div className="flex justify-between font-bold">
+              <span>ratings:</span>
+              <span>4.3</span>
+            </div>
+
+            <div className="flex justify-between">
+              <span className=" font-bold">Bat</span>
+              <span>Bowl</span>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <p className="font-bold">Price</p>
+              <button className="btn">Choose Player</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
