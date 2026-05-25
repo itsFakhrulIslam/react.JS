19b0b2746216b63a115ca21a9ece282846b6c290
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-05-25 15:06:13 +0600

    all players fetched and show the ui perfectly

--- module40/src/component/allPlayers/AllPlayers.jsx
+++ module40/src/component/allPlayers/AllPlayers.jsx
@@ -5,10 +5,10 @@ const AllPlayers = ({ onPromiseData }) => {
   //   console.log(onPromiseData);
 
   const allPlayersData = use(onPromiseData);
-  console.log(allPlayersData);
+  // console.log(allPlayersData);
 
   return (
-    <div className="max-w-300 mx-auto">
+    <div className="max-w-300 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <Player onPlayer={allPlayersData} />
     </div>
   );
--- module40/src/component/allPlayers/Player.jsx
+++ module40/src/component/allPlayers/Player.jsx
@@ -2,54 +2,68 @@ import userLogo from "../../assets/user.png";
 import flagLogo from "../../assets/flags.png";
 
 const Player = ({ onPlayer }) => {
-  console.log(onPlayer);
+  // console.log(onPlayer);
+
+  // const {
+  //   name,
+  //   country,
+  //   playing_role,
+  //   rating,
+  //   bat_side,
+  //   bowl_side,
+  //   price,
+  //   image_url,
+  // } = onPlayer;
 
   return (
     <>
-      <div>
-        <div className="card bg-white text-black w-96 shadow-sm p-6">
+      {onPlayer.map((player, index) => (
+        <div
+          className="card bg-white text-black w-96 shadow-sm p-6"
+          key={index}
+        >
           <figure>
             <img
               className="rounded-lg"
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
+              src={player.image_url}
+              alt={player.name}
             />
           </figure>
 
           <div className="border-b border-gray-300 py-2">
             <div className="flex items-center">
               <img className="w-10 -ml-2" src={userLogo} alt="user" />
-              <h2 className="card-title">Virat Kohli</h2>
+              <h2 className="card-title">{player.name}</h2>
             </div>
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <img className="w-7 -ml-1" src={flagLogo} alt="flagLogo" />
-                <h3>CountryName</h3>
+                <h3>{player.country}</h3>
               </div>
 
-              <button className="btn">All rounder</button>
+              <button className="btn">{player.playing_role}</button>
             </div>
           </div>
 
           <div className="space-y-2">
             <div className="flex justify-between font-bold">
               <span>ratings:</span>
-              <span>4.3</span>
+              <span>{player.rating}</span>
             </div>
 
             <div className="flex justify-between">
-              <span className=" font-bold">Bat</span>
-              <span>Bowl</span>
+              <span className=" font-bold">{player.bat_side}</span>
+              <span>{player.bowl_side}</span>
             </div>
 
             <div className="flex items-center justify-between">
-              <p className="font-bold">Price</p>
+              <p className="font-bold">Price: {player.price} Core</p>
               <button className="btn">Choose Player</button>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
 };
