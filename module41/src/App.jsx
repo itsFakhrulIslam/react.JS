0df4dc319b9b634192cc4e2fa0f9c9ecfd130b4e
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-03 12:43:27 +0600

    daisy ui setup done for faster coding and boom!!!

--- module41/src/App.jsx
+++ module41/src/App.jsx
@@ -3,7 +3,20 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-center">Learn React Module41</h1>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-4xl text-center">Learn React Module41</h1>
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
