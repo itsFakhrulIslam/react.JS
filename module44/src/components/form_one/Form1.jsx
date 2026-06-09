22c0342cd5f44697b08d6992ebb5be920f90f2c6
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-09 15:05:42 +0600

    simple form declared and test

--- module44/src/components/form_one/Form1.jsx
+++ module44/src/components/form_one/Form1.jsx
@@ -1,7 +1,14 @@
 const Form1 = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    console.log(`Hello, ${name}!`);
+    e.target.reset();
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Enter your name" />
         <button type="submit">Submit</button>
       </form>
