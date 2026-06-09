df8cec34ec77cf65cc650018a5f1bfe821f25129
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-09 13:17:50 +0600

    form learn by react

---
+++ module44/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Form1 from "./components/form_one/Form1";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Learn React module44</h1>
+          <h2>Explore the React form</h2>
+
+          {/* simple Form */}
+          <Form1 />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ module44/src/components/form_one/Form1.jsx
@@ -0,0 +1,12 @@
+const Form1 = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="name" placeholder="Enter your name" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form1;
