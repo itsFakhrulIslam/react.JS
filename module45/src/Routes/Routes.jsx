d2fa7f714eed764b6641bba1801ec1be08fa5fe7
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-11 14:10:32 +0600

    file & folder structure maintains here, and router path in router.jsx sperated by folder structuring

---
+++ module45/src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+// react router setups here
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home page</h1>,
+  },
+]);
--- module45/src/main.jsx
+++ module45/src/main.jsx
@@ -1,15 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router";
-
-// react router setups here
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <h1>Home page</h1>,
-  },
-]);
+import { RouterProvider } from "react-router";
+import { router } from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
