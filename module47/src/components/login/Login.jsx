ce36aa6e4525e2e665006d5bf9ec9f2f961d5d6f
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-14 12:55:53 +0600

    google authentication is done

--- module47/src/components/login/Login.jsx
+++ module47/src/components/login/Login.jsx
@@ -1,7 +1,29 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase/firebase.init_config";
+
+const gooleProvider = new GoogleAuthProvider();
+
 const Login = () => {
+  const handleGoogleSignIn = () => {
+    console.log("google btn click");
+
+    signInWithPopup(auth, gooleProvider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <h1>this is Login page</h1>
+      <button
+        onClick={handleGoogleSignIn}
+        className="border-2 border-amber-500 p-2 rounded-2xl hover:bg-blue-500 cursor-pointer"
+      >
+        Sign in with goolge
+      </button>
     </div>
   );
 };
