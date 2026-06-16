a76ddc42242b3713fcfb767c13947f71e874cc3b
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-16 15:09:25 +0600

    auth initialization and export the auths

--- module48/src/_info.js
+++ module48/src/_info.js
@@ -7,3 +7,15 @@
  * 2. second time visite, already have an account 
  *      sign in / log in (all similar)
  */
+
+/** firebase projects setup guide
+ * 1. firebase projects init/start/create
+ * 2. create web app in firebase
+ * 3. init in the projects by firebase
+ * 4. get firebase configuration code
+ * 5. authentication methods select
+ * 6. enabled auth method
+ * 7. go to selected auth documentation
+ * 8. initialization selected auths with export auth
+ * 9. 
+ */
--- module48/src/firebase/firebase.config.js
+++ module48/src/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,11 @@ const firebaseConfig = {
   projectId: "empassauth-2d499",
   storageBucket: "empassauth-2d499.firebasestorage.app",
   messagingSenderId: "1075949388003",
-  appId: "1:1075949388003:web:c255d88b383f43b43bc5d6"
+  appId: "1:1075949388003:web:c255d88b383f43b43bc5d6",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
