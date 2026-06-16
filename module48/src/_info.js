599eda42bab9f6a0214ac4c5815e5c2ac744a8af
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-16 13:13:48 +0600

    email/pass auth related note takes here

---
+++ module48/src/_info.js
@@ -0,0 +1,9 @@
+// email password authentication related topic here
+
+/**
+ * 1. first time visit, have not account
+ *      sign up / create accout / register (all similar)
+ * 
+ * 2. second time visite, already have an account 
+ *      sign in / log in (all similar)
+ */
