19f00af8ae3c58eb87fb470be4e0cfe985610b2b
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-06-18 22:57:34 +0600

    useEffect use in observer, because they memory killar in web for use useeffects

--- module49/src/contexts/authContext/AuthProvider.jsx
+++ module49/src/contexts/authContext/AuthProvider.jsx
@@ -5,6 +5,7 @@ import {
 } from "firebase/auth";
 import { AuthContext } from "./AuthContext";
 import { auth } from "../../firebase/firebase.config";
+import { useEffect } from "react";
 
 const AuthProvider = ({ children }) => {
   const createUser = (email, pass) => {
@@ -16,13 +17,18 @@ const AuthProvider = ({ children }) => {
   };
 
   //   get user info to firebase
-  onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) {
-      console.log("inside onAuthStateChanged- if", currentUser);
-    } else {
-      console.log("inside onAuthStateChanged- else", currentUser);
-    }
-  });
+  //   onAuthStateChanged(auth, (currentUser) => {
+  //     if (currentUser) {
+  //       console.log("inside onAuthStateChanged- if", currentUser);
+  //     } else {
+  //       console.log("inside onAuthStateChanged- else", currentUser);
+  //     }
+  //   });
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (currentUser) => {
+//       console.log(currentUser);
+//     });
+//   }, []);
 
   const authInfos = {
     createUser,
